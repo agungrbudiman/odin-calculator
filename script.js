75284d7fff536711ed9920a380c65f6bd894123a
Agung RB <agungrbudiman@gmail.com>
2022-07-27 10:27:35 +0700

    working basic calculator

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const display = document.querySelector("#display");
+let formula = {
+    n1: "",
+    n2: "",
+    op: null,
+};
+let displayData = "";
 
 function add(n1, n2) {
-    console.log(n1);
-    return n1 + n2;
+    return +n1 + +n2;
 }
 
 function subtract(n1, n2) {
@@ -17,17 +22,57 @@ function divide(n1, n2) {
     return n1 / n2;
 }
 
-function operate(operator, n1, n2) {
-    return operator(n1, n2);
+function operate() {
+    if (formula.op == null) return;
+    const result = window[formula.op](formula.n1, formula.n2);
+    const roundedResult = +result.toPrecision(9);
+    formula.n1 = result;
+    formula.n2 = '';
+    formula.op = null;
+    updateDisplay(roundedResult, true);
+    return result;
 }
 
-let displayData = "";
-function OutputToDisplay(e) {
-    displayData += e.target.innerText;
-    display.textContent = displayData;
+function updateDisplay(string, clear=false) {
+    if (clear) displayData = "";
+    displayData += string;
+    display.innerText = displayData;
+    if (clear == -1) display.innerText = '0';
+}
+
+function clear() {
+    updateDisplay('', -1);
+    formula.n1 = '';
+    formula.n2 = '';
+    formula.op = null;
+}
+
+function inputNumber(e) {
+    const number = e.target.innerText;
+    if (formula.op == null) {
+        if (formula.n1 == '' && number == '0') return; //prevent trailing zero
+        formula.n1 += number;
+    } else {
+        if (formula.n2 == '' && number == '0') return; //prevent trailing zero
+        formula.n2 += number;
+    } 
+    updateDisplay(e.target.innerText);
+    console.log(formula);
+}
+
+function inputOperator(e) {
+    const operator = e.target.getAttribute('data-function');
+    if (formula.op != null) operate(); // 2nd.. chain
+    formula.op = e.target.getAttribute('data-function');
+    updateDisplay(e.target.innerText);
+    console.log(formula);
 }
 
 const btnNumbers = document.querySelectorAll(".grid-item.number");
-btnNumbers.forEach(btnNumber => {
-    btnNumber.addEventListener('click', OutputToDisplay);
-});
+const btnOperators = document.querySelectorAll(".grid-item.operator");
+const btnResult = document.querySelector("#result");
+const btnClear = document.querySelector("#clear");
+btnNumbers.forEach(btnNumber => btnNumber.addEventListener('click', inputNumber));
+btnOperators.forEach(btnOperator => btnOperator.addEventListener('click', inputOperator));
+btnResult.addEventListener('click', operate);
+btnClear.addEventListener('click', clear);
